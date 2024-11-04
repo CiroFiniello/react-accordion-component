341f1297f9696ab919bbece41a0df4c198b361d2
CodeKiro <kirofiniello@outlook.it>
2024-11-04 17:44:38 +0100

    ho aggiunto la grafica di base, e creato uno state per la gestione degli accordion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
 import { useState } from "react";
 import "../App.css";
 
@@ -19,11 +21,29 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <Accordion title={el.title} text={el.text} num={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="item">
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
 }
